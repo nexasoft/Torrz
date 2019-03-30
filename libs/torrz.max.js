f49829ce0e30651818f876c08f374dec457c144f
Nexa™ <arilsonbrj@gmail.com>
2019-03-30 15:56:01 -0300

    Added Torrz Files

---
+++ libs/torrz.max.js
@@ -0,0 +1,8 @@
+var $ = jQuery = require('jquery')(window);
+$(function(){
+    alert("showbox")
+});
+
+$( window ).on( "load", function() {
+    console.log( "window loaded" );
+});
---
+++ torrz.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+let win;
+
+function createWindow () {
+  // Create the browser window.
+  win = new BrowserWindow({ width: 800, height: 600, webPreferences: {
+    contextIsolation: true,
+    webSecurity: true,
+    nodeIntegration: true
+  }})
+
+  win.webContents.openDevTools()
+
+  win.setMenu(null)
+
+  // and load the index.html of the app.
+  win.loadFile(__dirname+'/index.html')
+
+  win.on('closed', function () {
+    win = null
+  })
+
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  if (win === null) {
+    createWindow()
+  }
+})
